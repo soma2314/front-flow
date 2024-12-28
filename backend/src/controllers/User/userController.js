70828db389c39d2c5b4a7a727a12d122dacadd4f
Somanath Mikali <somanathmikali@Somanaths-MacBook-Air.local>
2024-12-28 22:00:52 +0530

    doneee

--- backend/src/controllers/User/userController.js
+++ backend/src/controllers/User/userController.js
@@ -30,39 +30,40 @@ export const loginController = async(req, res) => {
     if (!email || !password) {
         res.status(400).json({ message: ("All input are required, or in am not getting proper things from the frontend"), receivedDetails: req.body });
     }
-    const user = await User.findOne({ email: email });
-    if (!user) {
-        res.status(404).json({ message: ("User not found"), receivedDetails: req.body });
-    } else {
-        const isPasswordCorrect = await bcrypt.compare(password, user.password);
-        if (!isPasswordCorrect) {
-            res.status(401).json({ message: ("Incorrect password"), receivedDetails: req.body });
-        } else {
-            console.log("password correct");
-            const details = {
-                objectId: user._id,
-                name: user.name,
-                email: user.email,
-                role: user.role,
-            }
-            const token = generateToken(details);
-            console.log("token", token);
-            res.cookie("token", token, {
-                path: "/",
-                expires: new Date(Date.now() + 86400000), // 24 hours
-                secure: true, // Make sure this is enabled in production with HTTPS
-                httpOnly: true, // Prevent client-side JavaScript from accessing the cookie
-            });
-            console.log("Cookie set successfully");
+    res.status(200).json({ message: ("All input are there, or in am getting proper things from the frontend"), receivedDetails: req.body });
+    // const user = await User.findOne({ email: email });
+    // if (!user) {
+    //     res.status(404).json({ message: ("User not found"), receivedDetails: req.body });
+    // } else {
+    //     const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    //     if (!isPasswordCorrect) {
+    //         res.status(401).json({ message: ("Incorrect password"), receivedDetails: req.body });
+    //     } else {
+    //         console.log("password correct");
+    //         const details = {
+    //             objectId: user._id,
+    //             name: user.name,
+    //             email: user.email,
+    //             role: user.role,
+    //         }
+    //         const token = generateToken(details);
+    //         console.log("token", token);
+    //         res.cookie("token", token, {
+    //             path: "/",
+    //             expires: new Date(Date.now() + 86400000), // 24 hours
+    //             secure: true, // Make sure this is enabled in production with HTTPS
+    //             httpOnly: true, // Prevent client-side JavaScript from accessing the cookie
+    //         });
+    //         console.log("Cookie set successfully");
 
-            res.status(201).json({
-                message: "Login successful",
-                user: {
-                    email: user.email,
-                    role: user.role,
-                    name: user.name,
-                }
-            });
-        }
-    }
+    //         res.status(201).json({
+    //             message: "Login successful",
+    //             user: {
+    //                 email: user.email,
+    //                 role: user.role,
+    //                 name: user.name,
+    //             }
+    //         });
+    //     }
+    // }
 }
